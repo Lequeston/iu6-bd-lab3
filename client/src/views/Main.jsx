c85ea7d1eb3eb44bded49f561492e74720a7cce9
Fumi Hin <fumihin@outlook.com>
2021-12-10 23:37:57 +0300

    Добавил выбор городов отбытия и прибытия
    Только UI, без функционала.

--- client/src/views/Main.jsx
+++ client/src/views/Main.jsx
@@ -1,19 +1,49 @@
-import React from 'react';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Layout, Row, Col } from 'antd';
 
-import './Main.css'
+import CitySelect from '../components/CitySelect/CitySelect';
+
+import './Main.css';
 
 const { Header, Content, Footer } = Layout;
 
+const cityList = [
+  {id: 1, title: 'Москва'},
+  {id: 2, title: 'Мо'},
+  {id: 3, title: 'Молотов'},
+  {id: 4, title: 'Молодечно'},
+]
+
 const Main = () => {
+  const [departureCity, setDepartureCity] = useState();
+  const [arrivalCity, setArrivalCity] = useState();
+
   return (
     <Layout className="layout">
       <Header>
         <div className="logo" />
-        <Menu theme="dark" mode="horizontal">
-        </Menu>
       </Header>
       <Content style={{ padding: '0 50px' }}>
+        <div className="site-layout-content">
+          <Row gutter={16}>
+            <Col span={6}>
+              <CitySelect
+                placeholder="Откуда"
+                setCity={setDepartureCity}
+                cityList={cityList}
+              />
+            </Col>
+            <Col span={6}>
+              <CitySelect
+                placeholder="Куда"
+                setCity={setArrivalCity}
+                cityList={cityList}
+              />
+            </Col>
+            <Col span={6}></Col>
+            <Col span={6}></Col>
+          </Row>
+        </div>
         <div className="site-layout-content">Content</div>
       </Content>
       <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
