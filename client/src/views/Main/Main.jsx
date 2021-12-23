4890e75c7144f1e439178cfccc4aa8e19cbe655e
Fumi Hin <fumihin@outlook.com>
2021-12-23 19:16:55 +0300

    Добавил список обратных полётов

--- client/src/views/Main/Main.jsx
+++ client/src/views/Main/Main.jsx
@@ -16,29 +16,70 @@ const {Content } = Layout;
 const Main = ({
 }) => {
   const [passengerAmount, setPassengerAmount] = useState();
+  const [flightClass, setFlightClass] = useState()
+  const [roundtrip, setRoundtrip] = useState(false);
+  const [dates, setDates] = useState();
+  const [departureCity, setDepartureCity] = useState();
+  const [arrivalCity, setArrivalCity] = useState();
 
   const {
-    flights,
-    limit,
-    page,
-    length,
-    setEndCity,
-    setStartCity,
-    setDate,
-    setAddFlightId,
-    setFlightClass,
-    handleSetPage
+    flights: dirFlights,
+    limit: dirLimit,
+    page: dirPage,
+    length: dirLength,
+    setEndCity: setDirEndCity,
+    setStartCity: setDirStartCity,
+    setDate: setDirDate,
+    setAddFlightId: setDirAddFlightId,
+    setFlightClass: setDirFlightClass,
+    handleSetPage: setDirHandleSetPage
+  } = useFetchFlightsApi();
+
+  const {
+    flights: retFlights,
+    limit: retLimit,
+    page: retPage,
+    length: retLength,
+    setEndCity: setRetEndCity,
+    setStartCity: setRetStartCity,
+    setDate: setRetDate,
+    setAddFlightId: setRetAddFlightId,
+    setFlightClass: setRetFlightClass,
+    handleSetPage: setRetHandleSetPage
   } = useFetchFlightsApi();
 
   const { cities } = useFetchCities();
 
   const { flightClassList } = useFetchFlightClassList();
 
-  const onDatesChange = (dates) => {
+  useEffect(() => {
     if (dates) {
-      setDate(dates[0])
+      if (dates[1]) {
+        setRoundtrip(true);
+        setDirDate(dates[0]);
+        setRetDate(dates[1]);
+      }
+      else {
+        setRoundtrip(false);
+        setDirDate(dates[0]);
+      }
     }
-  };
+  }, [dates]);
+
+  useEffect(() => {
+    setDirStartCity(departureCity);
+    setRetEndCity(departureCity);
+  }, [departureCity]);
+
+  useEffect(() => {
+    setDirEndCity(arrivalCity);
+    setRetStartCity(arrivalCity);
+  }, [arrivalCity]);
+
+  useEffect(() => {
+    setDirFlightClass(flightClass);
+    setRetFlightClass(flightClass)
+  }, [flightClass])
 
   return (
       <Content style={{ padding: '0 50px' }}>
@@ -49,20 +90,20 @@ const Main = ({
                 <CitySelect
                   placeholder="Откуда"
                   data={cities}
-                  onChange={setStartCity}
+                  onChange={setDepartureCity}
                 />
               </Col>
               <Col span={5}>
                 <CitySelect
-                  placeholder="Куда"
+                  placeholderhandleSetPage="Куда"
                   data={cities}
-                  onChange={setEndCity}
+                  onChange={setArrivalCity}
                 />
               </Col>
               <Col span={7}>
                 <DatePicker
                   placeholder={['Туда', 'Обратно']}
-                  onChange={onDatesChange}
+                  onChange={setDates}
                 />
               </Col>
               <Col span={7}>
@@ -75,14 +116,28 @@ const Main = ({
             </Row>
           </div>
           <div style={{ minHeight: 'calc(100vh - 64px - 70px - 104px - 50px)' }} className="site-layout-content">
-            <FlightList
-              data={flights}
-              length={length}
-              setPage={handleSetPage}
-              limit={limit}
-              page={page}
-              setAddFlightId={setAddFlightId}
-            />
+            <Row>
+              <Col>
+                <FlightList
+                  data={dirFlights}
+                  length={dirLength}
+                  setPage={setDirHandleSetPage}
+                  limit={dirLimit}
+                  page={dirPage}
+                  setAddFlightId={setDirAddFlightId}
+                />
+              </Col>
+              <Col>
+                <FlightList
+                  data={retFlights}
+                  length={retLength}
+                  setPage={setRetHandleSetPage}
+                  limit={retLimit}
+                  page={retPage}
+                  setAddFlightId={setRetAddFlightId}
+                />
+              </Col>
+            </Row>
           </div>
         </div>
       </Content>
