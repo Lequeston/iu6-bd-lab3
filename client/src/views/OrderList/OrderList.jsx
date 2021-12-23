d6ff7f5f9cbd3e2b41ecfb2efea3b74efe4e4aae
Fumi Hin <fumihin@outlook.com>
2021-12-23 22:19:32 +0300

    Причесал список заказов

--- client/src/views/OrderList/OrderList.jsx
+++ client/src/views/OrderList/OrderList.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Layout, List, Row, Col, Typography, Image, Empty } from 'antd';
+import { Layout, List, Row, Col, Typography } from 'antd';
 import dayjs from 'dayjs';
 
 import './OrderList.css'
 import useFetchOrdersApi from '../../hooks/useFetchOrdersApi';
 
 const { Content } = Layout;
+const { Text } = Typography;
 
 const OrderList = () => {
   const {
@@ -35,7 +36,6 @@ const OrderList = () => {
     <Content
       style={{
         padding: '0 50px',
-        width: '1000px',
         margin: 'auto'
       }}
     >
@@ -49,31 +49,33 @@ const OrderList = () => {
           renderItem={item => (
             <List.Item
               key={item.id}
+              
             >
-                  <Row gutter={[16, 0]} justify="center">
-                    <Col>{item.route.airDeparture.city}, {item.route.airDeparture.title}</Col>
-                    <Col>
-                      <Row>{dayjs(item.airDepartureData).format('HH:mm')}</Row>
-                      <Row>{dayjs(item.airDepartureData).format('DD MMMM')}</Row>
-                    </Col>
-                    <Col >
-                      <Row>{printTime(...convertHours(calcDifference(dayjs(item.airDepartureData), dayjs(item.airArrivalData))))}</Row>
-                      <Row>{'--------------------------------->'}</Row>
-                    </Col>
-                    <Col>
-                      <Row>{dayjs(item.airArrivalData).format('HH:mm')}</Row>
-                      <Row>{dayjs(item.airArrivalData).format('DD MMMM')}</Row>
-                    </Col>
-                    <Col>{item.route.airArrival.city}, {item.route.airArrival.title}</Col>
-                  </Row>
-                  <Row gutter={[16, 0]}>
-                    <Col>{item.flightNumber}</Col>
-                    <Col>
-                      <Row>{item.planeType}</Row>
-                      <Row>{item.comfortClass} класс</Row>
-                    </Col>
-                    <Col>{item.price}</Col>
-                  </Row>
+              <Row gutter={[16, 0]} justify="space-between">
+                <Col><Text strong style={{ fontSize: '20px' }}>{item.route.airDeparture.city}</Text>, {item.route.airDeparture.title}</Col>
+                <Col>
+                  <Row>{dayjs(item.airDepartureData).format('HH:mm')}</Row>
+                  <Row>{dayjs(item.airDepartureData).format('DD MMMM')}</Row>
+                </Col>
+                <Col >
+                  <Row justify="center">{printTime(...convertHours(calcDifference(dayjs(item.airDepartureData), dayjs(item.airArrivalData))))}</Row>
+                  <Row>{'--------------------------------->'}</Row>
+                </Col>
+                <Col>
+                  <Row>{dayjs(item.airArrivalData).format('HH:mm')}</Row>
+                  <Row>{dayjs(item.airArrivalData).format('DD MMMM')}</Row>
+                </Col>
+                <Col><Text strong style={{ fontSize: '20px' }}>{item.route.airArrival.city}</Text>, {item.route.airArrival.title}</Col>
+              </Row>
+              <div style={{height: '20px'}}></div>
+              <Row gutter={[16, 0]}>
+                <Col>{item.flightNumber}</Col>
+                <Col>
+                  <Row>{item.comfortClass} класс</Row>
+                  <Row><Text type="secondary">{item.planeType}</Text></Row>
+                </Col>
+                <Col>{item.price}</Col>
+              </Row>
             </List.Item>
           )}
         />
